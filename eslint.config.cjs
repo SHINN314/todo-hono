a5b8846a4243a6c35d94b02d5377e6bcbbf582ef
SHINN <shinnosukekimura8@gmail.com>
2026-01-14 01:09:13 +0900

    update: eslintrcをeslint.config.cjsにまいぐれーとした

---
+++ eslint.config.cjs
@@ -0,0 +1,53 @@
+const {
+    defineConfig,
+} = require("eslint/config");
+
+const globals = require("globals");
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        sourceType: "module",
+        ecmaVersion: "latest",
+
+        parserOptions: {
+            tsconfigRootDir: __dirname,
+            project: ["./tsconfig.eslint.json"],
+        },
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ),
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            argsIgnorePattern: "^_",
+        }],
+
+        "@typescript-eslint/no-explicit-any": "warn",
+    },
+}]);
