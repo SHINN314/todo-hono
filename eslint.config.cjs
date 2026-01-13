44a9d145f4b98089c8e93ad261b5160f8cfa71d6
SHINN <shinnosukekimura8@gmail.com>
2026-01-14 01:37:01 +0900

    update: eslintとformatterを同時にかけれるようにした

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -6,50 +6,36 @@ const globals = require("globals");
 const tsParser = require("@typescript-eslint/parser");
 const typescriptEslint = require("@typescript-eslint/eslint-plugin");
 const js = require("@eslint/js");
+const prettierConfig = require("eslint-config-prettier");
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-module.exports = defineConfig([{
-    languageOptions: {
-        globals: {
-            ...globals.node,
+module.exports = [
+    js.configs.recommended,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.es2024,
+            },
+            parser: tsParser,
+            sourceType: "module",
+            ecmaVersion: "latest",
+            parserOptions: {
+                tsconfigRootDir: __dirname,
+                project: ["./tsconfig.eslint.json"],
+            },
         },
-
-        parser: tsParser,
-        sourceType: "module",
-        ecmaVersion: "latest",
-
-        parserOptions: {
-            tsconfigRootDir: __dirname,
-            project: ["./tsconfig.eslint.json"],
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+        rules: {
+            ...typescriptEslint.configs.recommended.rules,
+            ...typescriptEslint.configs["recommended-requiring-type-checking"].rules,
+            "@typescript-eslint/no-unused-vars": ["warn", {
+                argsIgnorePattern: "^_",
+            }],
+            "@typescript-eslint/no-explicit-any": "warn",
         },
     },
-
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    extends: compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "prettier",
-        "prettier/@typescript-eslint",
-    ),
-
-    rules: {
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            argsIgnorePattern: "^_",
-        }],
-
-        "@typescript-eslint/no-explicit-any": "warn",
-    },
-}]);
+    prettierConfig,
+];
