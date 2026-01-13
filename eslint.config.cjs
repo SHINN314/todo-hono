cbdba9513a27c7c0076796e5806842a3e45b24b1
SHINN <shinnosukekimura8@gmail.com>
2026-01-14 01:19:25 +0900

    add: prettierを導入

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -41,6 +41,8 @@ module.exports = defineConfig([{
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier",
+        "prettier/@typescript-eslint",
     ),
 
     rules: {
