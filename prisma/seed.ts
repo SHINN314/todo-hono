291d6bb8137f9ac917963b415b8b562a9c9c3074
SHINN <shinnosukekimura8@gmail.com>
2026-01-13 18:54:28 +0900

    update: ハードコードで書き直し

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,35 +9,60 @@ const prisma = new PrismaClient({
     adapter,
 });
 
-const userData: Prisma.UserCreateInput[] = [
-    {
-        email: "example@gmail.com",
-        name: "hgoe1",
-        posts: {
-            create: [
-                {
-                    title: "Join the Prisma Discord",
-                    content: "https://pris.ly/discord",
-                    published: true,
-                },
-                {
-                    title: "Prisma on YouTube",
-                    content: "https://pris.ly/youtube",
-                }
-            ]
+async function main() {
+    // clear existing data
+    await prisma.user.deleteMany({});
+    await prisma.post.deleteMany({});
+
+    // create users
+    const alice = await prisma.user.create({
+        data: {
+            email: "alice@example.com",
+            name: "Alice",
         }
-    },
-    {
-        email: "bob@prisma.io",
-        name: "Bob",
-        posts: {
-            create: [
-                {
-                    title: "Follow Prisma on Twitter",
-                    content: "https://www.twitter.com/prisma",
-                    published: true,
-                }
-            ]
+    });
+
+    const bob = await prisma.user.create({
+        data: {
+            email: "bob@example.com",
+            name: "Bob",
         }
-    }
-];
+    });
+
+    // create posts
+    // alice's posts
+    await prisma.post.createMany({
+        data: [
+            {
+                title: "Join the Prisma discord",
+                content: "https://pris.ly/discord",
+                published: true,
+                authorId: alice.id,
+            },
+            {
+                title: "Prisma  on YouTube",
+                content: "https://pris.ly/youtube",
+                authorId: alice.id,
+            }
+        ]
+    })
+
+    // bob's posts
+    await prisma.post.create({
+        data: {
+            title: "Follow Prisma on Twitter",
+            content: "https://www.twitter.com/prisma",
+            published: true,
+            authorId: bob.id
+        }
+    })
+}
+
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    })
