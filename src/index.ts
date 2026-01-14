aba4b8c6f8e8021ee8390eba799c788b210c4900
SHINN <shinnosukekimura8@gmail.com>
2026-01-14 12:00:29 +0900

    add: userのデータをデータベースから取得するエンドポイントを作成

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import type { PrismaClient } from "./generated/prisma/client";
+import withPrisma from "./lib/prisma";
+import prismaConfig from "../prisma.config";
 
 type ContextWithPrisma = {
   Variables: {
@@ -12,5 +14,14 @@ const app = new Hono<ContextWithPrisma>();
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+// user get endpoint
+app.get("/users", withPrisma, async (c) => {
+  const prisma = c.get("prisma");
+  const users = await prisma.user.findMany({
+    include: {posts: true},
+  });
+  return c.json({users})
+})
 
 export default app;
