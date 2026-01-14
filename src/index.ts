a6c651f0dc5a5b6f81962059b7b97de815975b81
SHINN <shinnosukekimura8@gmail.com>
2026-01-14 11:56:56 +0900

    update: prismaをセットできるようにした

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Hono } from "hono";
+import type { PrismaClient } from "./generated/prisma/client";
 
-const app = new Hono();
+type ContextWithPrisma = {
+  Variables: {
+    prisma: PrismaClient;
+  };
+};
+
+const app = new Hono<ContextWithPrisma>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
